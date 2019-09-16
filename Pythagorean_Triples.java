f7be24ff1b27035f5409226fe6045a99a99f3623
Rami-Ismael <39520903+Rami-Ismael@users.noreply.github.com>
2019-09-16 15:39:29 -0500

    Update and rename VladikandFraction.java to Pythagorean_Triples.java

--- Pythagorean_Triples.java
+++ Pythagorean_Triples.java
@@ -2,7 +2,7 @@ package com.company.Main;
 
 import java.util.Scanner;
 
-public class VladikandFraction {
+public class Pythagorean_Triples {
 
     public static void main(String[] args) {
 	// write your code here
